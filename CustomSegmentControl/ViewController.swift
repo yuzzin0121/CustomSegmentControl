8dd425024946385e9e09a9e3ad69811bb7151664
yuzzin <ydbwlsjki60@gmail.com>
2025-02-24 10:08:16 +0900

    Initial Commit

---
+++ CustomSegmentControl/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomSegmentControl
+//
+//  Created by 조유진 on 2/24/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
